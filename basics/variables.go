af2195a731a4a55a8095df3e2ba29f6d6461db1c
Rafael Lino <rafael.veloso.lino@hotmail.com>
2023-04-13 22:02:51 -0300

    some adjustments

---
+++ basics/variables.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func variablesTest() {
+	//var example int = 3 //
+	//example = 4
+	c := "sample"
+	var b = "sex"
+	c = b
+	var name string // default value of "", number default 0 and other nil
+	var sex rune    // default 0
+	sex = 'a'       // prints the number representation
+	fmt.Print(c)
+	fmt.Print(name)
+	fmt.Print("-----------")
+	fmt.Print(sex)
+	fmt.Print("-----------")
+	j, b := "13", "14" // comma, ok idiom allows to reassign
+	fmt.Printf(b)
+	fmt.Printf(j)
+	// camelCase
+	const MaxSpeed = 30 // cannot reassign
+}
---
+++ comments.md
@@ -0,0 +1,17 @@
+// rune = alias for int32 that represents a letter
+// string is a series bytes | String are not null terminated
+// "" <- string
+// ” <- rune
+// `let's code\n` <- raw string
+
+// go commands:
+// build -> distribute
+// build -race -> check for concurrency problems
+// run -> runs directly, no output executable
+// mod -> manage modules and dependencies , mod tidy -> update dependencies
+// test -> runs the projects test
+// fmt -> format all source files
+
+//------------
+
+// variables -> Name, data, etc
