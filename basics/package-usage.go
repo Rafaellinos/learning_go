6cd5cb8aff684ad31da04656283e2f83b34a1c29
Rafael Lino <rafael.veloso.lino@hotmail.com>
2023-04-30 22:39:24 -0300

    add new learning package

---
+++ basics/package-usage.go
@@ -0,0 +1,7 @@
+package main
+
+import brutils "github.com/Rafaellinos/br-utils"
+
+func examplePackage() {
+	brutils.FormatCnpj("12345678901234")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module learning_go
 
 go 1.20
+
+require github.com/Rafaellinos/br-utils v0.2.0
